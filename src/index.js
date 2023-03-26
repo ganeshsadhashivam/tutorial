a19546312d69c28f19ef719c38a1a9a489f0e5ef
ganesh <ganeshsadhashivam@outlook.com>
2023-03-27 00:33:00 +0530

    nesting components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Greeting = () => {
+  return (
+    <>
+      <h1>MyFirst component</h1>
+      <ul>
+        <li>
+          <a>hello world</a>
+        </li>
+      </ul>
+      <Person />
+      <Message />
+    </>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//nesting componenets
+const Person = () => <h2>john</h2>;
+
+const Message = () => <h2>hi hello</h2>;
+
+//we can create react element like this also
+// const Greeting = () => {
+//   return React.createElement("h2", {}, "hello World");
+// };
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Greeting />);
